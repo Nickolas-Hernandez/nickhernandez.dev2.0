abd39fe398f5edf9847699e34ac896f1702cca33
Nick Hernandez <xnickhernandez@gmail.com>
2025-07-22 16:32:43 -0700

    add unstyled nav

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,8 +1,14 @@
 <template>
     <div class="">
-        <div class="">
-            <p>Header</p>
-        </div>
+        <nav class="navigation-bar">
+            <ul class="nav-links">
+                <li class="nav-link">Home</li>
+                <li class="nav-link">About</li>
+                <li class="nav-link">Projects</li>
+                <li class="nav-link">Blog</li>
+                <li class="nav-link">Contact</li>
+            </ul>
+        </nav>
             <slot />
         <div class="">
             <p>Footer</p>
