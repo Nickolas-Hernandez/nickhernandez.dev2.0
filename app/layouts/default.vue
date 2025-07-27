d2e892393a470e831ab72f079a5be77c2543e912
Nick Hernandez <xnickhernandez@gmail.com>
2025-07-26 23:07:35 -0700

    Add links to nav items

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,22 +1,26 @@
 <script setup lang="ts">
-import { NuxtLink } from '#components';
 import type { NavigationMenuItem } from '@nuxt/ui';
 
 const navItems = ref<NavigationMenuItem[]>([
     {
-        label: 'home'
+        label: 'home',
+        to: '/'
     },
     {
-        label: 'About'
+        label: 'About',
+        to: '/about'
     },
     {
-        label: 'Projects'
+        label: 'Projects',
+        to: '/projects'
     },
     {
-        label: 'Blog'
+        label: 'Blog',
+        to: '/blog'
     },
     {
-        label: 'Contact'
+        label: 'Contact',
+        to: '/contact'
     }
 ])
 
@@ -26,16 +30,6 @@ const navItems = ref<NavigationMenuItem[]>([
     <div class="">
         <header>
               <UNavigationMenu :items="navItems" class="w-full justify-center" />
-
-            <!-- <nav class="navigation-bar">
-                <ul class="nav-links">
-                    <li class="nav-link"><NuxtLink to="/">Home</NuxtLink></li>
-                    <li class="nav-link"><NuxtLink to="/about">About</NuxtLink></li>
-                    <li class="nav-link"><NuxtLink to="/projects">Projects</NuxtLink></li>
-                    <li class="nav-link"><NuxtLink to="/blog">Blog</NuxtLink></li>
-                    <li class="nav-link"><NuxtLink to="/contact">Contact</NuxtLink></li>
-                </ul>
-            </nav> -->
         </header>
         <main>
             <slot />
