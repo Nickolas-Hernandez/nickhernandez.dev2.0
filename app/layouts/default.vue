e5f80a9294c5b6cf8833a53cf72b3075d3d23e9d
Nick Hernandez <xnickhernandez@gmail.com>
2025-07-26 22:21:12 -0700

    update

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -5,19 +5,22 @@ import { NuxtLink } from '#components';
 
 <template>
     <div class="">
-        <nav class="navigation-bar">
-            <ul class="nav-links">
-                <li class="nav-link"><NuxtLink to="/">Home</NuxtLink></li>
-                <li class="nav-link"> <NuxtLink to="/about">About</NuxtLink></li>
-                <li class="nav-link"> <NuxtLink to="/projects">Projects</NuxtLink></li>
-                <li class="nav-link"> <NuxtLink to="/blog">Blog</NuxtLink></li>
-                <li class="nav-link"> <NuxtLink to="/contact">Contact</NuxtLink></li>
-            </ul>
-        </nav>
+        <header>
+            <nav class="navigation-bar">
+                <ul class="nav-links">
+                    <li class="nav-link"><NuxtLink to="/">Home</NuxtLink></li>
+                    <li class="nav-link"><NuxtLink to="/about">About</NuxtLink></li>
+                    <li class="nav-link"><NuxtLink to="/projects">Projects</NuxtLink></li>
+                    <li class="nav-link"><NuxtLink to="/blog">Blog</NuxtLink></li>
+                    <li class="nav-link"><NuxtLink to="/contact">Contact</NuxtLink></li>
+                </ul>
+            </nav>
+        </header>
+        <main>
             <slot />
-        <div class="">
+        </main>
+        <footer class="">
             <p>Footer</p>
-        </div>
+        </footer>
     </div>
 </template>
-
