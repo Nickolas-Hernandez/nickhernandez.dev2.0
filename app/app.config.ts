9d099f707a073b06fd1fa97bce0176464792db96
Nick Hernandez <xnickhernandez@gmail.com>
2025-08-03 16:04:29 -0700

    Add hero buttons and avatar

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,37 +1,49 @@
 export default defineAppConfig({
   // https://ui.nuxt.com/getting-started/theme#design-system
   ui: {
-    colors: {
-      primary: '#2B2B2B',
-      primaryHover: '#1f1f1f',
-      primarySoft: '#3C3C3C',
+    color: {
+      primary: '2B2B2B',
+      primaryHover: '1f1f1f',
+      primarySoft: '3C3C3C',
 
-      accent: '#F3E8D9',
-      accentHover: '#e8dac5',
-      accentSubtle: '#f9f5f0',
+      accent: 'F3E8D9',
+      accentHover: 'e8dac5',
+      accentSubtle: 'f9f5f0',
 
-      backgroundLight: '#FFFFFF',
-      backgroundSubtle: '#F8F6F3',
-      borderGray: '#DCDCDC',
+      backgroundLight: 'FFFFFF',
+      backgroundSubtle: 'F8F6F3',
+      borderGray: 'DCDCDC',
 
-      textBody: '#EAEAEA',
-      textMuted: '#A8A8A8',
-      textDarkOnLight: '#2B2B2B',
-      textAccent: '#C7B6A0',
+      textBody: 'EAEAEA',
+      textMuted: 'A8A8A8',
+      textDarkOnLight: '2B2B2B',
+      textAccent: 'C7B6A0',
 
-      highlight: '#B5835A',
-      success: '#A0C49D',
-      info: '#9BB1C7'
-
-    },
-    button: {
-      defaultVariants: {
-        // Set default button color to neutral
-        // color: 'neutral'
-      }
+      highlight: 'B5835A',
+      success: 'A0C49D',
+      info: '9BB1C7'
     },
     container: {
         base: 'w-full max-w-(--ui-container) mx-auto px-4 sm:px-6 lg:px-8'
     }
-  }
+  },
+  footer: {
+        credits: `Copyright © ${new Date().getFullYear()}`,
+        links: [{
+            'icon': 'i-simple-icons-github',
+            'to': 'https://github.com/Nickolas-Hernandez',
+            'target': '_blank',
+            'aria-label': 'Nick Hernandez on GitHub'
+            }, {
+            'icon': 'i-simple-icons-linkedin',
+            'to': 'https://www.linkedin.com/in/nickolashernandez/',
+            'target': '_blank',
+            'aria-label': 'Nick Hernandez on Discord'
+            }, {
+            'icon': 'i-simple-icons-x',
+            'to': 'https://x.com/xnickhernandez',
+            'target': '_blank',
+            'aria-label': 'Nick Hernandez on X'
+        }]
+    }
 })
--- app/components/Hero.vue
+++ app/components/Hero.vue
@@ -1,8 +1,37 @@
+<script setup>
+const { footer } = useAppConfig()
+</script>
+
 <template>
     <section class="text-center pt-16">
+        <UAvatar src="https://github.com/Nickolas-Hernandez.png" size="3xl" icon="i-lucide-image" alt="Nickolas Hernandez" />
         <h1 class="tracking-tight font-semi-bold text-pretty text-3xl sm:text-4xl lg:text-5xl text-shadow-md max-w-lg mx-auto text-center">Hello! I&apos;m Nick Hernandez Web Developer</h1>
         <p>I build custom websites where clean design meets powerful functionality and AI-driven solutions. Based in California, I bring ideas to life with code, creativity, and a focus on real business results.</p>
         <!-- Available for new project button -->
+            <UButton 
+                class="mt-4" 
+                color="success"                
+                variant="ghost"
+                size="lg" 
+                href="/contact"
+                label="Available for new projects" 
+            >
+                <template #leading>
+                     <span class="relative flex size-2">
+                        <span
+                            class="absolute inline-flex size-full rounded-full opacity-75 bg-success animate-ping"
+                        />
+                        <span
+                            class="relative inline-flex size-2 scale-90 rounded-full bg-success"
+                        />
+                        </span>
+                </template>
+            </UButton>
          <!-- Github, Linkedin, and Twitter links -->
+          <div class="gap-x-4 inline-flex mt-4">
+            <div  v-for="(link, index) in footer?.links" :key="index">
+                <UButton v-bind="{ size: 'md', color: 'neutral', variant: 'ghost', ...link }" />
+            </div>
+          </div>
     </section>
 </template>
