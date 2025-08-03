5215814a1c278bb7907d0a71b4afc129c739a77f
Nick Hernandez <xnickhernandez@gmail.com>
2025-08-02 19:39:11 -0700

    Add color theme to app.config.ts

--- app/app.config.ts
+++ app/app.config.ts
@@ -2,8 +2,27 @@ export default defineAppConfig({
   // https://ui.nuxt.com/getting-started/theme#design-system
   ui: {
     colors: {
-      primary: 'emerald',
-      neutral: 'slate',
+      primary: '#2B2B2B',
+      primaryHover: '#1f1f1f',
+      primarySoft: '#3C3C3C',
+
+      accent: '#F3E8D9',
+      accentHover: '#e8dac5',
+      accentSubtle: '#f9f5f0',
+
+      backgroundLight: '#FFFFFF',
+      backgroundSubtle: '#F8F6F3',
+      borderGray: '#DCDCDC',
+
+      textBody: '#EAEAEA',
+      textMuted: '#A8A8A8',
+      textDarkOnLight: '#2B2B2B',
+      textAccent: '#C7B6A0',
+
+      highlight: '#B5835A',
+      success: '#A0C49D',
+      info: '#9BB1C7'
+
     },
     button: {
       defaultVariants: {
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,6 @@
 <template>
-  <h1>This is the Home page</h1>
+    <div class="text-center">
+        <h1 class="tracking-tight font-semi-bold text-pretty text-3xl sm:text-4xl lg:text-5xl text-shadow-md max-w-lg mx-auto text-center">Hello! I&apos;m Nick Hernandez Web Developer</h1>
+        <p>I build custom websites where clean design meets powerful functionality. Based in California, I bring ideas to life with code, creativity, and a focus on real business results.</p>
+    </div>
 </template>
